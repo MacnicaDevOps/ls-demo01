c6b0bf46cb93c74c98c6c278340d4e1d78f341ea
minepicco <minepicco@gmail.com>
2023-04-11 10:47:18 +0900

    test12

--- getPrismaData.sh
+++ getPrismaData.sh
@@ -2,7 +2,7 @@
 
             app_name="PRISMA_SCAN_${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BUILD_NUM}"
             app_priority="H"
-            curl -k -u "${pc_user}:${pc_pass}" -H "Content-Type: application/json" "${pc_url}/api/v1/scans?search=${imagename}/${CIRCLE_PROJECT_REPONAME,,}:${CIRCLE_BUILD_NUM}" | jq -r '[ .[].entityInfo ]' > "ci_scan.json"
+            curl -k -u "${pc_user}:${pc_pass}" -H "Content-Type: application/json" "${pc_url}/api/v1/scans?search=buildimage:temp" | jq -r '[ .[].entityInfo ]' > "ci_scan.json"
 
             echo "------- LeanSeeksのアップロードデータを生成中"
             vuln_data='[{"id": "ci_scan.json","scanner": 0,"payload":'
