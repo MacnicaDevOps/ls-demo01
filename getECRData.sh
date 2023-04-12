1b4c4f5bd98485b90de88215ed201b11c4a3cb70
minepicco <minepicco@gmail.com>
2023-04-12 15:32:22 +0900

    test19

--- getECRData.sh
+++ getECRData.sh
@@ -2,6 +2,11 @@
 
 source /tmp/workspace/env.txt
 
+echo "デバッグ用"
+ls -lah /tmp/workspace
+cat /tmp/workspace/env.txt
+
+
 # LeanSeeksの環境変数を指定してファイルに書き出す
 echo "app_name=ECR_SCAN_${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BUILD_NUM}">param.txt
 echo 'app_priority="H"'>>param.txt
