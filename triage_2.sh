e424ee15d3888a8771e7947c384f5b72a22d2a16
minepicco <minepicco@gmail.com>
2023-04-17 14:42:10 +0900

    test32

--- triage_2.sh
+++ triage_2.sh
@@ -9,11 +9,13 @@
             s3_url=`echo "${cred}" | jq .uploadDestination.url | sed -e 's/\"//g'`
             s3_jwt=`echo "${cred}" | jq .uploadDestination.key | sed -e 's/\"//g'`
             echo "------- LeanSeeksのアップロードデータを生成中"
+            cat ci_scan.json | jq
             vuln_data='[{"id": "ci_scan.json","scanner": 0,"payload":'
             vuln_data+=$(cat "ci_scan.json")
             vuln_data+="}]"
             echo "------- データをLeanSeeksにアップロード中"
             echo "${vuln_data}" > vuln_data.json
+            cat vuln_data.json | jq
             curl -X 'PUT' "${s3_url}" --data-binary @vuln_data.json
             echo "------- トリアージリクエストパラメーターの準備中"
             param='{"application_name":"'${app_name}'","importance":"'${app_priority}'","is_template":false,"pods":'
