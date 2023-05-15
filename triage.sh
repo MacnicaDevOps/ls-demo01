3c25a260d4cf305522bcafc4d2546202bb9555d2
minepicco <minepicco@gmail.com>
2023-05-15 18:29:40 +0900

    test

--- triage.sh
+++ triage.sh
@@ -26,11 +26,11 @@ echo "App_Priority: ${app_priority}"
 
 param="{ application_name: ${app_name}, importance: ${app_priority}, is_template: false, pods:"
 param+=$(jq -R -s -f mapping.jq params.csv | jq -r -c '[.[] |select(.pod_name != null and .is_root != "is_root" )]'| sed -e 's/"¥r"//g')"}"
-echo ${param} | sed 's/"TRUE"/true/g' | sed -e 's/"FALSE"/false/g' | sed -e 's/\r//g'> "param.json"
+echo ${param} | sed 's/"TRUE"/true/g' | sed -e 's/"FALSE"/false/g' > "param.json"
 
 echo "デバッグ"
 echo "param.jsonの中身"
-cat param.json | jq
+cat param.json 
 
 # トリアージリクエストを実行する
 echo "------- トリアージリクエスト実行中"
